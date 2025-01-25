8849bf72bc6c47c1e9d0090975efdb24e9d9dc5e
trantiendat12345 <trantiendatas1@gmail.com>
2025-01-25 16:08:51 +0700

    tiendat

---
+++ src/main/java/com/example/tiendat/databases/seeder/DatabaseSeeder.java
@@ -0,0 +1,14 @@
+package com.example.tiendat.databases.seeder;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseSeeder implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Something Seeding ........");
+    }
+    
+}

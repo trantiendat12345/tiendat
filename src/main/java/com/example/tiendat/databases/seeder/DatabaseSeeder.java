5a96b5e0a3fcc20bb144e5387aedb80480283a6a
trantiendat12345 <trantiendatas1@gmail.com>
2025-02-04 15:37:41 +0700

    java

--- src/main/java/com/example/tiendat/databases/seeder/DatabaseSeeder.java
+++ src/main/java/com/example/tiendat/databases/seeder/DatabaseSeeder.java
@@ -2,13 +2,44 @@ package com.example.tiendat.databases.seeder;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.EntityManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class DatabaseSeeder implements CommandLineRunner {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Something Seeding ........");
+
+        if (isTableEmpty()) {
+
+            String password = passwordEncoder.encode("password");
+            entityManager.createNativeQuery("INSERT INTO users (name, email, password, user_catalogue_id, phone) VALUES (?, ?, ?, ?, ?)")
+                    .setParameter(1, "Tien Dat")
+                    .setParameter(2, "trantiendat@gmail.com")
+                    .setParameter(3, password)
+                    .setParameter(4, 1)
+                    .setParameter(5, "1203012301203")
+                    .executeUpdate();
+
+            System.out.println("password: " + password);
+        } 
+
+    }
+
+    private boolean isTableEmpty() {
+        Long count = (Long) entityManager.createQuery("SELECT COUNT(id) FROM User").getSingleResult();
+        return count == 0;
     }
     
 }
--- src/main/java/com/example/tiendat/modules/users/entities/User.java
+++ src/main/java/com/example/tiendat/modules/users/entities/User.java
@@ -1,5 +1,121 @@
 package com.example.tiendat.modules.users.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Column;
+import java.time.LocalDateTime;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(name = "user_catalogue_id")
+    private Long UserCatalogueId;
+
+
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private String address;
+    private String image;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist //goi den ham nay truoc khi insert du lieu vao database
+    protected void onCreated() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate //goi den ham nay truoc khi update du lieu vao database
+    protected void onUpdated() {
+        updatedAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserCatalogueId() {
+        return UserCatalogueId;
+    }
+
+    public void setUserCatalogueId(Long userCatalogueId) {
+        this.UserCatalogueId = userCatalogueId;
+    }
+
+    public String getName() {
+        return name;
+    }
     
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
 }
